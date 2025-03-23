0966573e785a7bbe56e7e723d4eeef3c6e5462e2
yashanshh <yashansh726@gmail.com>
2025-03-23 21:17:11 +0530

    init

---
+++ bin.ts
@@ -0,0 +1,16 @@
+import cluster from 'cluster'
+import os from 'os'
+import { app } from './index'
+const numCPUs = os.cpus().length;
+
+if(cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  for(let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(3000, () => {
+    console.log(`Worker ${process.pid} started.`)
+  });
+}
---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import os from 'os'
+export const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/cpu', (req, res) => {
+  for(let i = 0; i < 100000000; i++) {
+    Math.random();
+  }
+  res.send('Hello World!');
+});
+
+app.get('/host', (req, res) => {
+  res.send(os.hostname());
+});
